7dd3dc1a70c23506a509d7b370d25e1fb8380a55
suhyun <qkrtngus116@naver.com>
2023-06-07 16:24:37 +0900

    section3

--- springsecsection3/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
+++ springsecsection3/src/main/java/com/eazybytes/config/EazyBankUserDetails.java
@@ -9,18 +9,23 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class EazyBankUserDetails implements UserDetailsService {
 
     @Autowired
     CustomerRepository customerRepository;
+
+    //UserDetailsService를 활용하여 고객테이블 구현
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         String userName, password = null;
         List<GrantedAuthority> authorities = null;
+        //파라미터에 기반한 username을 기준으로 username에 해당하는 이메일을 customer에 저장
         List<Customer> customer = customerRepository.findByEmail(username);
         if (customer.size() == 0) {
             throw new UsernameNotFoundException("User details not found for the user : " + username);
--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -30,8 +30,9 @@ public class ProjectSecurityConfig {
         secured: myAccount, myBalance, myLoans, myCards,
         not secured: notices, contact
          */
-        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
-                        .requestMatchers("/notices","/contact").permitAll())
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+                        .requestMatchers("/notices","/contact","/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
@@ -74,10 +75,10 @@ public class ProjectSecurityConfig {
     권장하지 않음.
      */
 
-    @Bean
+    /*@Bean
     public UserDetailsService userDetailsService(DataSource dataSource){
         return new JdbcUserDetailsManager(dataSource);
-    }
+    }*/
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
