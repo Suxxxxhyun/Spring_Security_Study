d3de83fc66d61b8ccd9eef7d3e6debee3d2d6c2e
suhyun <qkrtngus116@naver.com>
2023-06-07 14:19:55 +0900

    section3

--- springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,7 +6,16 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -26,6 +35,51 @@ public class ProjectSecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
 
+    /*@Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+
+        /*사용자 생성 방식 1 - withDefaultPasswordEncoder() 사용 (안전하지 않기때문에 권장하지 않음)*/
+
+        /*UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);*/
+
+        /*사용자 생성 방식 2 - NoOpPasswordEncoder Bean을 이용 (안전하지 않기때문에 권장하지 않음)*/
+
+        /*UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin")
+                .build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }*/
+
+    /*
+    NoOpPasswordEncoder는
+    사용자가 암호를 입력할때마다 일반 텍스트로 받기 때문에
+    (즉, 패스워드를 실제로 인코딩하지 않음)
+    권장하지 않음.
+     */
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
